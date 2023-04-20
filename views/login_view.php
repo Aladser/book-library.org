f7da1fdde5f0c213ee6c7d3fd1da7a94ad6390ea
Aladser <aladser@gmail.com>
2023-04-20 13:02:26 +0900

    новый дизайн окна входа 1

--- views/login_view.php
+++ views/login_view.php
@@ -1,31 +1,21 @@
 <!-- модальное окно входа -->
 <container id='loginInputSection' class='modal'>
-    <article class='modalWindow loginWindow'>
+    <container class='modalWindow loginWindow'>
         <input type='button' class='modalWindow__closeBtn' id='modalWindow__closeBtn' value='x'>
         <h3 class='loginWindow__header'> Авторизация</h3>
 
         <form method="POST" class='loginWindow__form' id='loginWindow__form'>
-
-            <p class='loginWindow__formRow'>
-                <label for="loginInput" class='loginWindow__label'>Логин:</label>
-                <input type='text' class='loginWindow__input loginWindow__loginInput' name='db_login' id='loginInput' autocomplete='on' value ='user' placeholder="Логин">
-            </p> 
-
-            <p class='loginWindow__formRow'>
-                <label for="passwordInput" class='loginWindow__label'>Пароль:</label>
-                <input type="password" class='loginWindow__input loginWindow__passwordInput' name='password' id='passwordInput' autocomplete='off' value= 'user@@user' placeholder="Пароль">
-            </p>
-
-            <p class='loginWindow__formRow loginWindow__btnRow'> 
-                <input type='submit' class='loginWindow__btn' id='loginWindow__sendBtn' value='Войти'>
-                <input type='button' class='loginWindow__btn' id='loginWindow__regBtn' value='Регистрация'>            
-            </p>
-
+            <input type='text' class='loginWindow__input loginWindow__loginInput' name='db_login' id='loginInput' value ='user' placeholder="Логин">
+            <br>
+            <input type="password" class='loginWindow__input loginWindow__passwordInput' name='password' id='passwordInput' value= 'user@@user' placeholder="Пароль">
+            <br>
+            <input type='submit' class='loginWindow__btn' id='loginWindow__sendBtn' value='Войти'>
+            <br>
+            <input type='button' class='loginWindow__btn' id='loginWindow__regBtn' value='Регистрация'> 
+            <br>
             <input type="checkbox" class='loginWindow__saveAuth' id="loginWindow__saveAuth" name="saveAuth" checked/>
-            <label for="loginWindow__saveAuth">Запомнить меня</label>
-            
-            <p class='loginWindow__error hidden' id='loginWindow__error'>Ошибка</p>
-            
+            <label for="loginWindow__saveAuth">Запомнить меня</label>   
+            <p class='loginWindow__error hidden' id='loginWindow__error'>Ошибка</p>         
             <? // auth и токен ?>
             <input type="hidden" name="auth" value=1>
             <input type="hidden" name="token" value="<?=$token?>">
@@ -37,5 +27,5 @@
             <input type="hidden" name="token" value="<?=$token?>">
             <input type="submit" class='authvkForm__submit' value=''>
         </form>
-    </article>
+    </container>
 </container>
